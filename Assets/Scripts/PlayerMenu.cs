7a4527b0b628de91ff1ec101e8d0e7e7a5d5b0d0
crigge90 <alexanderros90@gmail.com>
2018-06-17 11:49:57 +0200

    Transfered files to GIT

---
+++ Assets/Scripts/PlayerMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMenu : MonoBehaviour {
+
+    public GameObject CharacterMenu;
+    public CharacterManager playerManager;
+    public Text playerMenuStats;
+    void Start ()
+    {
+        Debug.Log(playerManager.PlayerNationality);
+        Debug.Log(playerManager.PlayerTrauma);
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CharacterMenu.SetActive(!CharacterMenu.activeInHierarchy);
+        }
+        playerMenuStats.text = "Nationality:\n" + playerManager.PlayerNationality
+                               + "\n\n" + "Health:\n" + playerManager.healthStatus + 
+                               "\n\n Injury type:\n" + playerManager.PlayerTrauma;
+    }
+}
---
+++ Assets/Scripts/characterManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+    public int health;
+    public string healthStatus;
+    public enum Nationality {british, french, belgian, german}
+    
+    public enum Trauma {bulletTorso, bulletLeg, bulletArm, shrapnelTorso, shrapnelLeg, shrapnelArm, infectionTorso, infectionLeg, infectionArm}
+    Trauma trauma;
+    
+    public Nationality PlayerNationality
+    {
+        get; set;
+    }
+
+    public Trauma PlayerTrauma
+    {
+        get; set;
+    }
+    public void Health ()
+    {
+        if (health == 100)
+        {
+            healthStatus = "Healthy";
+        }
+        if (health <= 80)
+        {
+            healthStatus = "Trauma";
+        }
+        if (health <= 50)
+        {
+            healthStatus = "Serious trauma";
+        }
+        if (health <= 20)
+        {
+            healthStatus = "Deadly trauma";
+        }
+    }
+
+	void Awake ()
+    {
+
+        int nationalityResult = UnityEngine.Random.Range(0, (int)Enum.GetValues(typeof(Nationality)).Length);
+        Debug.Log(nationalityResult);
+        PlayerNationality = (Nationality)nationalityResult;
+        int traumaResult = UnityEngine.Random.Range(0, (int)Enum.GetValues(typeof(Trauma)).Length);
+        PlayerTrauma = (Trauma)traumaResult;
+        health = 49;
+        Health();
+	}
+}
