f298127d264c33d327e95887cd3bb62c3bae8b9a
crigge90 <alexanderros90@gmail.com>
2018-06-17 11:57:05 +0200

    Added text parser script

---
+++ Assets/TextDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TextCollection
+{
+    public string key;
+    public string value;
+}
+
+[CreateAssetMenu(fileName = "Text_EN.asset", menuName = "Create Text Asset")]
+public class TextDatabase : ScriptableObject
+{
+    [SerializeField]
+    public TextCollection[] collection;
+
+    public string GetText(string key)
+    {
+        string key_lower = key.ToLower();
+
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (key_lower == collection[i].key.ToLower())
+                return collection[i].value;
+        }
+
+        Debug.LogWarning("Attempt to get string with key: " + key + " failed!");
+        return "";
+    }
+}
---
+++ Assets/TextExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextExample : MonoBehaviour
+{
+    [SerializeField]
+    TextDatabase text;
+
+	void Start ()
+    {
+        Debug.Log(text.GetText("test1"));
+        Debug.Log(text.GetText("TEST1"));
+        Debug.Log(text.GetText("tEsT1"));
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
