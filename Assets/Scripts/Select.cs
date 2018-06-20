a45bfd8339f4ba480daeafdc2fe359a87dec5f72
Alexander Ros <alexanderros90@gmail.com>
2018-06-20 23:03:48 +0200

    Added background canvas and select script with a test object

---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour {
+
+    public GameObject currentObject = null;
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray cameraRay = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit cameraHit;
+
+        if (Physics.Raycast(cameraRay, out cameraHit))
+        {
+            //Debug.Log(cameraHit.collider.gameObject + ": You found me!");
+            if (Input.GetMouseButtonUp(0))
+            {
+                currentObject = cameraHit.collider.gameObject;
+                Debug.Log(cameraHit.collider.gameObject + ": You clicked me!");
+            }
+        }
+    }
+}
--- Assets/Scripts/sceneManager.cs
+++ Assets/Scripts/sceneManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour {
 
+     
 	void Start ()
     {
-        
-	}
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+    }
 	void Update ()
     {
        
