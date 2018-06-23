74d50616bebfe6d671a3b130579cdef3879d8451
crigge90 <alexanderros90@gmail.com>
2018-06-23 21:46:32 +0200

    Created room prefab and minor player menu changes

---
+++ Assets/Scripts/RoomName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomName : MonoBehaviour {
+    // REMEBER TO CHANGE NAMESPACE AFTER THE ROOMS NAME!!! Like: RoomExample
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
