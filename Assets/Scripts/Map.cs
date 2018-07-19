8f29e7da95b308a8624067b58cac7552f9062f27
crigge90 <alexanderros90@gmail.com>
2018-07-19 23:07:45 +0200

    Worked on mapasset

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject Destination1;
+    public GameObject Destination2;
+    public GameObject Destination3;
+    public GameObject Destination4;
+    public GameObject Destination5;
+    public GameObject Destination6;
+    public GameObject Destination7;
+    public GameObject Destination8;
+    public GameObject Destination9;
+    public GameObject Destination10;
+    public GameObject Destination11;
+    public GameObject Destination12;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
